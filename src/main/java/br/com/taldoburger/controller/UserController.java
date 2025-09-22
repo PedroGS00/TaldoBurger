3d2ad94e388543eb3fe3f8ed5f84f724c8278641
Pedro Gabriel <pedro.gsilva0108@gmail.com>
2025-09-22 17:35:36 -0300

    Adiciona configuração de segurança e endpoint para listar usuários

--- src/main/java/br/com/taldoburger/controller/UserController.java
+++ src/main/java/br/com/taldoburger/controller/UserController.java
@@ -5,10 +5,9 @@ import br.com.taldoburger.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -31,4 +30,11 @@ public class UserController {
         else
             return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
 }
